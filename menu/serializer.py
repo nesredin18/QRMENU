5a9b2f66b71a5a78693070b7b348b9a4a2212eb7
nas <negusienesredin@gmail.com>
2024-02-04 22:44:09 -0800

    requirments

--- menu/serializer.py
+++ menu/serializer.py
@@ -8,16 +8,17 @@ class MenuSerializer(serializers.ModelSerializer):
         model = Menu
         fields = ['id', 'restaurant', 'title', 'description', 'is_active', 'restaurant_id']
         read_only_fields = ['id', 'restaurant']
-        
+
 
 class MenuItemImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItemImage
         fields = ['id', 'menu_item', 'image_url', 'description']
+        
 
 class MenuItemSerializer(serializers.ModelSerializer):
     images = MenuItemImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = MenuItem
-        fields = ['id', 'menu', 'name', 'description', 'price', 'images']
+        fields = ['id', 'menu', 'name', 'description', 'price']
